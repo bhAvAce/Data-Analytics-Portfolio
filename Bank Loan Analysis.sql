17a3082b5b097e01b62634ec6ee9e629b6eb7522
bhAvAce <146638473+bhAvAce@users.noreply.github.com>
2024-08-24 00:01:45 +0530

    Added Power BI, Excel, Tableau, and SQL files using Git LFS

---
+++ Bank Loan Analysis.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c32672c20550079ffe3f5956934760c5e536bee20f7a846322ac129a34eea31c
+size 2143
---
+++ Olist_store_Analysis.sql
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b787eaa489f39e6300108ca3f8b32a58cc3d7054255e87af83f3f8ae2b032b92
+size 6243
